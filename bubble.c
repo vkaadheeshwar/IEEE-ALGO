02f17b4777de6c5f447c83c028510e60ab90eaa9
vkaadheeshwar <vkaadheeshwar95@gmail.com>
2014-08-04 00:47:31 +0530

    Create bubble.c
    Bubble sort

---
+++ bubble.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main( )
+{
+     int a[100]; 
+     int i, j, temp, n ;
+     printf("How many numbers you want to sort : \n");
+     scanf("%d",&n);
+     printf("Enter %d number values you want to sort\n", n);
+     for(j=0; j<n; j++)
+     scanf("%d",&a[j]);
+     
+     for(j=1;j<n;j++)
+     {
+          for(i=0; i<n; i++)
+          {
+               if(a[i]>a[i+1])
+               {
+                     temp=a[i];
+                     a[i]=a[i+1];
+                     a[i+1]=temp;
+               } 
+          }
+     }
+     
+     printf ( "\n\nArray after sorting:\n") ;
+    
+     for ( i = 0 ; i <n ; i++ )
+		printf ( "%d\t", a[i] ) ;
+ }
